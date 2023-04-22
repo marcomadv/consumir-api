50048c304a8aaeeb13a5feea171af1cbdfda5335
marcomadv <imarcoma@gmail.com>
2023-04-22 12:33:26 +0200

    agregar clase Exchange y su test

--- appcoints/models.py
+++ appcoints/models.py
@@ -1,5 +1,8 @@
 import requests
 
+class ModelError(Exception):
+    pass
+
 class AllCoinApiIo:
     def __init__(self):
         self.lista_criptos=[]
@@ -20,3 +23,18 @@ class AllCoinApiIo:
             else:
                 self.lista_no_criptos.append(item["asset_id"])
 
+class Exchange:
+    def __init__(self, cripto):
+        self.moneda_cripto = cripto
+        self.rate = None
+        self.status_code = None
+
+    def updateExchange(self,apikey):
+        r = requests.get(f"https://rest.coinapi.io/v1/exchangerate/{self.moneda_cripto}/EUR?apikey={apikey}")
+
+        respuesta = r.json()
+        self.status_code = r.status_code
+        if r.status_code == 200:
+            self.rate = respuesta['rate']
+        else:
+            raise ModelError(f"status: {r.status_code}, error: {respuesta['error']}") 
--- test_models.py
+++ test_models.py
@@ -1,5 +1,6 @@
-from appcoints.models import AllCoinApiIo
+from appcoints.models import *
 from appcoints.config import APIKEY
+import pytest
 
 def test_allcoin():
     todo = AllCoinApiIo()
@@ -7,3 +8,14 @@ def test_allcoin():
     todo.getAllCoins(APIKEY)
     total = len(todo.lista_criptos) + len(todo.lista_no_criptos)
     assert total == 16379
+
+def test_exchange_ok():
+    cambio = Exchange("ETH")
+    cambio.updateExchange(APIKEY)
+    assert cambio.rate != None
+
+def test_exchange_error():
+    cambio2 = Exchange("ÑÑ")
+    with pytest.raises(ModelError) as exceptionInfo:    
+        cambio2.updateExchange(APIKEY)
+    assert cambio2.status_code == 550
